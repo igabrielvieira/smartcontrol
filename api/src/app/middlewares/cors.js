512e956616fc007317bfed3be3ab0ed82cee50d7
Gabriel Vieira <loucodoroyale@gmail.com>
2023-10-26 08:48:47 -0300

    Solved CORS problem

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,5 +1,6 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://192.168.3.6:3000');
+  // response.setHeader('Access-Control-Allow-Origin', 'http://192.168.3.6:3000');
+  response.setHeader('Access-Control-Allow-Origin', '*'); // Permitindo requisições de todos as origens
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '10');
